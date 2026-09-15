85d793f89a71f3aed7a0083a6ff004a845dceb9c
Steven Umuhoza <stevenumuhoza@gmail.com>
2026-09-15 19:40:22 +0200

    all is well pushing my next exercise

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,6 @@
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
-import "./index.css"
 import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
+import App from "./App"
 
-function Page() {
-    return (
-        <>
-            <Header />
-            <MainContent />
-            <Footer />
-        </>
-    )
-}
-
-root.render(
-    <Page />
+createRoot(document.getElementById("root")).render(
+    <App />
 )
